2b4aa26a5d953c75ea3db34dd18c57b2f7fa3c76
thrushalex <thrushalex@gmail.com>
2023-08-08 18:47:36 -0400

    Update default link

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/v1/routes', trains);
+app.use('/api/v1/', trains);
 app.use('*', (req, res) => {
     res.status(404).json({ error: 'not found' });
 });

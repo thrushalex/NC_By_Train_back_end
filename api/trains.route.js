57a7026d9d90a1d8789a6d013c04451b6328d6a7
thrushalex <thrushalex@gmail.com>
2023-08-08 18:50:22 -0400

    Update routes to reflect new functionality:

--- api/trains.route.js
+++ api/trains.route.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import RoutesController from './routes.controller.js';
 // import TicketsController from './tickets.controller.js';
-// import TimeTablesContoller from './timetables.controller.js';
+import TimeTablesContoller from './timetables.controller.js';
 
 const router = express.Router(); // Get access to Express router
 
-router.route('/').get(RoutesController.apiGetRoutes);
+router.route('/routes').get(RoutesController.apiGetRoutes);
+router.route('/routes/names').get(RoutesController.apiGetRouteNames);
+
+router.route('/timetables').get(TimeTablesContoller.apiGetTimetables);
+router.route('/timetables/routename/:routename').get(TimeTablesContoller.apiGetTimetablesByRoute);
 
 export default router;
